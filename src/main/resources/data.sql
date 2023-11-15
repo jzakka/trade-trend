b610b88bfaa60645b561599aeedae3311330f98a
Jzakka <mouse4786@gmail.com>
2023-11-15 14:55:09 +0900

    데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,19 @@
 INSERT INTO CLOTHES(MAIN_IMG, BRAND, MAKER, NAME, COLOR, CATEGORY, PRICE, DETAIL)
-VALUES ('/test/main1.jpeg', 'NIKE', 'ABCmart', 'jordan','#1f2f3e', 'shoes', 43000, '/test/detail1.jpeg');
-INSERT INTO CLOTHES(MAIN_IMG, BRAND, MAKER, NAME, COLOR, PRICE, DETAIL)
-VALUES ('/test/main2.jpeg', 'NIKE', 'musinsa', 'terrex','#1f2f3e', 21000, '/test/detail2.jpeg');
-INSERT INTO CLOTHES(MAIN_IMG, BRAND, MAKER, NAME, COLOR, PRICE, DETAIL)
-VALUES ('/test/main3.jpeg', 'ADIDAS', 'naver', 'superstar','#1f2f3e', 34000, '/test/detail3.jpeg');
+VALUES ('jordan-styles-kr.jpg', 'NIKE', 'ABCmart', 'jordan','#1f2f3e', 'shoes', 43000, '/test/detail1.jpeg');
+INSERT INTO CLOTHES(MAIN_IMG, CATEGORY,  BRAND, MAKER, NAME, COLOR, PRICE, DETAIL)
+VALUES ('acg-gore-tex-mountain-fly-shoes-78bQ8Z.png','shoes', 'NIKE', 'musinsa', 'terrex','#1f2f3e', 21000, '/test/detail2.jpeg');
+INSERT INTO CLOTHES(MAIN_IMG, CATEGORY, BRAND, MAKER, NAME, COLOR, PRICE, DETAIL)
+VALUES ('Black_EG4959_01_standard.jpg', 'shoes', 'ADIDAS', 'naver', 'superstar','#1f2f3e', 34000, '/test/detail3.jpeg');
+
+INSERT INTO CLOTHES(MAIN_IMG, CATEGORY,  BRAND, MAKER, NAME, COLOR, PRICE, DETAIL, LINK)
+VALUES ('AJNFMX7584_BLK_view.jpg', 'jacket','ARCTERYX', 'ARCTERYX', '[FW23]베타 재킷 남성', 'black', 650000, 'AJNFMX7584_00_view.jpg', 'https://arcteryx.co.kr/goods/view?no=683539');
+INSERT INTO CLOTHES(MAIN_IMG, CATEGORY, BRAND, MAKER, NAME, COLOR, PRICE, DETAIL, LINK)
+VALUES ('VJNSMX6693_AST_view.jpg', 'shirt', 'ARCTERYX', 'ARCTERYX', '[SS23] 페르마 LS 크루 남성', 'white', 530000, 'VJNSMX6693_00_view.jpg', 'https://arcteryx.co.kr/goods/catalog?code=000100010001');
+INSERT INTO CLOTHES(MAIN_IMG, CATEGORY, BRAND, MAKER, NAME, COLOR, PRICE, DETAIL, LINK)
+VALUES ('8f749119241b1cbcf151c3db877935b6.jpg', 'pants', 'LEE', 'LEE', '트위치 로고 와이드 기모 스웻팬츠 그레이', 'grey', 63200, 'LE2304PT22GR_02.jpg', 'https://leekorea.co.kr/product/detail.html?product_no=3616&cate_no=389&display_group=1');
+INSERT INTO CLOTHES(MAIN_IMG, CATEGORY, BRAND, MAKER, NAME, COLOR, PRICE, DETAIL, LINK)
+VALUES ('ca8aaf404c8b458b999ec5d5db105c4d.jpg', 'jacket', 'LEE', 'LEE', '리버시블 플리스 후드 집업 자켓 카키','khaki', 143200, 'LE2303JK02KH_02_2.jpg', 'https://leekorea.co.kr/product/detail.html?product_no=3648&cate_no=377&display_group=1');
+INSERT INTO CLOTHES(MAIN_IMG, CATEGORY, BRAND, MAKER, NAME, COLOR, PRICE, DETAIL, LINK)
+VALUES ('J1GC230401-1.jpg', 'shoes', 'MIZUNO', 'MIZUNO', 'WAVE RIDER 27 SW(SUPER WIDE)_J1GC2304', 'neon', 149000, 'J1GC2304-D4_1.jpg', 'https://kor.mizuno.com/kr-kr/wave+rider+27+sw%28super+wide%29_j1gc2304/KRJ1GC2304.html?dwvar_KRJ1GC2304_color=01&cgid=044001011#start=5');
+INSERT INTO CLOTHES(MAIN_IMG, CATEGORY, BRAND, MAKER, NAME, COLOR, PRICE, DETAIL, LINK)
+VALUES ('32YD302409-1.jpg', 'pants', 'MIZUNO', 'MIZUNO', 'WOVEN JOGGER PANTS_32YD3024', 'black', 69300, '32YD3024-D2.jpg', 'https://kor.mizuno.com/kr-kr/woven+jogger+pants_32yd3024/KR32YD3024.html?dwvar_KR32YD3024_color=09&cgid=044002015#start=13&sz=12');
