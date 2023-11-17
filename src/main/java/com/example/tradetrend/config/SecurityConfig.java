e521a22750dfa70b1bfb4a1fcbca70fae5ff3879
Jzakka <mouse4786@gmail.com>
2023-11-17 19:55:20 +0900

    feat: cors 설정

--- src/main/java/com/example/tradetrend/config/SecurityConfig.java
+++ src/main/java/com/example/tradetrend/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -12,9 +17,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
         return http
-                .cors(corsConfig -> corsConfig.disable())
+                .cors(cors -> cors.configurationSource(corsConfiguration()))
                 .csrf(csrfConfig -> csrfConfig.disable())
                 .authorizeHttpRequests(authConfig -> authConfig.anyRequest().permitAll())
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOrigin("tradetrend.store");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
